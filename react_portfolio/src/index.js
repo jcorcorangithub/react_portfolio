a144a40f91c9980eaca9338a8d9e6c21849ac8d8
James Corcoran <jpcsoccer1234@gmail.com>
2021-07-16 14:35:11 -0400

    added components folder

--- react_portfolio/src/index.js
+++ react_portfolio/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//may want to import the min.css 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
