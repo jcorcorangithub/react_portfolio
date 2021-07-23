75fb1b8ba82084146d78b75923bdab0d9a1d616a
James Corcoran <jpcsoccer1234@gmail.com>
2021-07-23 18:55:08 -0400

    added to projects section

--- react_portfolio/src/components/projects.js
+++ react_portfolio/src/components/projects.js
@@ -1,5 +1,27 @@
+
+//     {
+//         name: "Satellite Locator Application",
+//         gitLink: "https://github.com/Captain63/satellite-tracker",
+//         liveLink: null,
+//         screenshotPath: "/assets/images/satellite-locator-750px.png"
+//     },
+// ]
+
 export default function Projects(){
     return (
-    <p>projects1 project2 project3</p>
+        <div>
+            <h2>Password Generator</h2>
+            <a href="https://jcorcorangithub.github.io/password_generator_refactor/"> deployed app</a>
+            <a href="https://github.com/jcorcorangithub/password_generator_refactor"> - project Repo</a>
+
+            <h2>Coding Quiz</h2>
+            <a href="https://jcorcorangithub.github.io/coding_quiz_challenge/"> deployed app</a>
+            <a href="https://github.com/jcorcorangithub/coding_quiz_challenge"> - project Repo</a>
+
+            <h2>Covid Data</h2>
+            <a href="https://dmitrybalduev.github.io/covid-data-center/"> deployed app</a>
+            <a href="https://github.com/dmitrybalduev/covid-data-center"> - project Repo</a>
+            
+        </div>
     );
 }
