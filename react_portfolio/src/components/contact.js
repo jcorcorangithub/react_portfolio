6aaddb1adb4739db2e9d0dba5a5b04d3a1af94b8
James Corcoran <jpcsoccer1234@gmail.com>
2021-07-23 19:11:35 -0400

    added content to contact section

--- react_portfolio/src/components/contact.js
+++ react_portfolio/src/components/contact.js
@@ -1,3 +1,22 @@
 export default function Contact(){
-    return (<p>contacts page</p>);
+    return (
+        <div>
+                <h2>Contact Me</h2>
+                <form>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input type="text" className="form-control" id="name" placeholder="Your name"  required></input> {/*onBlur={requiredMessage}*/}
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" className="form-control" id="email" placeholder="Your email" required></input> {/*onBlur={validEmail}*/}
+                    </div>
+                    <div className="form-group">
+                        <label>Message</label>
+                        <textarea rows="3" className="form-control" id="textarea" placeholder="Add your message" required></textarea> {/*onBlur={requiredMessage}*/}
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>                
+            </div>
+    );
 }
