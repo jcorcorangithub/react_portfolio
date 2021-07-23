94b37b3e30b2e7c11c4ef49a8923fb8c0d1b129c
James Corcoran <jpcsoccer1234@gmail.com>
2021-07-23 18:01:42 -0400

    added links to footer and added an about me section

--- react_portfolio/src/components/aboutme.js
+++ react_portfolio/src/components/aboutme.js
@@ -2,7 +2,10 @@
 
 export default function AboutMe(){
     return (
-        <p>this is information about me</p>
+        <p>
+            My name is James Corcoran. I was born and raised on Long Island, NY and lived there for 21 years before moving to FLorida to pursue a Bachelor's degree in Forensic Psychology. At Florida Institute of Technology I continued to pursue a Psy.D. in CLinical Psychology but after realizing I was not interested in clinical work I left the program with a Masters degree and eventually started learning to code on my own. I moved back home and after learning Java and bits of other coding languages like HTML and CSS (just kidding, these are not coding languages. My current instructor would be furious if I thought they were), I decided to pursue a job in software development. A friend recommended I look into The George Washington University's bootcamp and eventually I applied and fortunately got accepted. My primary interests pertain to Psycholgy and Neuroscience (anything about the brain and behavior) and in the future I would love to find a way to apply my software development skills to that field.
+
+        </p>
     );
 }
 
